2e5c211514b15064ba1b4e3f9364b09b706f97e6
Nathan O <Nathan O>
2020-10-07 09:53:32 -0400

    Make HSL colors use 16-bit ints for components
     - Change the HSLColor struct to use 3 16-bit ints rather than 3
       float32's, in order to save memory for huge images.
    
     - In addition to the change to 16-bit values, the HSLColor now is
       defined as a slice, which allows the HSLImage to be defined using a
       single slice of 16-bit values, rather than a slice of structs, to
       ensure that no space is wasted on allocating padding between structs.

--- image_combiner_hsl.go
+++ image_combiner_hsl.go
@@ -21,40 +21,30 @@ import (
 // Implements the color interface. The output image will be composed of this.
 // Implements the color interface. All three components are expected to be in
 // the range [0.0, 1.0].
-type HSLColor struct {
-	H float32
-	S float32
-	L float32
+
+// Implements the color interface. Stores the H, S, and L components,
+// respectively. *This will panic if the slice doesn't contain at least 3
+// components.* Values after the first 3 are ignored. Each component is a
+// fraction out of 0xffff.
+type HSLColor []uint16
+
+// Utility function to convert the 3 16-bit values to fractional components.
+func (c HSLColor) HSLComponents() (float64, float64, float64) {
+	h := float64(c[0]) / float64(0xffff)
+	s := float64(c[1]) / float64(0xffff)
+	l := float64(c[2]) / float64(0xffff)
+	return h, s, l
 }
 
 func (c HSLColor) String() string {
-	return fmt.Sprintf("(%f, %f, %f)", c.H, c.S, c.L)
+	h, s, l := c.HSLComponents()
+	return fmt.Sprintf("(%f, %f, %f)", h, s, l)
 }
 
 // Converts a given arbitrary RGB color to a single brightness value.
-func convertToBrightness(c color.Color) float32 {
+func convertToBrightness(c color.Color) float64 {
 	r, g, b, _ := c.RGBA()
-	return float32(r+g+b) / (3.0 * 65535.0)
-}
-
-// This function sets the H value of the given HSLColor, returning the modified
-// HSLColor. It's written this way, rather than as a method of HSLColor, so
-// that the function can be passed to HSLImage's setComponent method.
-func setH(c HSLColor, value color.Color) HSLColor {
-	c.H = convertToBrightness(value)
-	return c
-}
-
-// See setH
-func setS(c HSLColor, value color.Color) HSLColor {
-	c.S = convertToBrightness(value)
-	return c
-}
-
-// See setH
-func setL(c HSLColor, value color.Color) HSLColor {
-	c.L = convertToBrightness(value)
-	return c
+	return float64(r+g+b) / (3.0 * 65535.0)
 }
 
 func clamp(v float64) float64 {
@@ -67,6 +57,29 @@ func clamp(v float64) float64 {
 	return v
 }
 
+// Linearly maps a floating point value in [0, 1] to [0, 0xffff]. Clamps the
+// value if it's not in [0, 1].
+func scaleTo16Bit(v float64) uint16 {
+	return uint16(clamp(v) * float64(0xffff))
+}
+
+// This function sets the H value of the given HSLColor. It's written this way,
+// rather than as a method of HSLColor, so that the function can be passed to
+// HSLImage's setComponent method.
+func setH(c HSLColor, value color.Color) {
+	c[0] = scaleTo16Bit(convertToBrightness(value))
+}
+
+// See setH
+func setS(c HSLColor, value color.Color) {
+	c[1] = scaleTo16Bit(convertToBrightness(value))
+}
+
+// See setH
+func setL(c HSLColor, value color.Color) {
+	c[2] = scaleTo16Bit(convertToBrightness(value))
+}
+
 // Returns R, G, B, given a particular hue value.
 func hueToRGB(h float64) (float64, float64, float64) {
 	r := math.Abs((h*6.0)-3.0) - 1.0
@@ -75,33 +88,27 @@ func hueToRGB(h float64) (float64, float64, float64) {
 	return clamp(r), clamp(g), clamp(b)
 }
 
-// Linearly maps a floating point value in [0, 1] to [0, 0xffff].
-func scaleTo16Bit(v float64) uint32 {
-	tmp := uint32(v * float64(0xffff))
-	if tmp > 0xffff {
-		tmp = 0xffff
-	}
-	return tmp
-}
-
 // I based this code off of the snippet here:
 // https://gist.github.com/mathebox/e0805f72e7db3269ec22
 func (c HSLColor) RGBA() (r, g, b, a uint32) {
-	r1, g1, b1 := hueToRGB(float64(c.H))
-	chroma := (1.0 - math.Abs(2.0*float64(c.L)-1)) * float64(c.S)
-	l := float64(c.L)
+	h, s, l := c.HSLComponents()
+	r1, g1, b1 := hueToRGB(h)
+	chroma := (1.0 - math.Abs(2.0*l-1)) * s
 	r1 = (r1-0.5)*chroma + l
 	g1 = (g1-0.5)*chroma + l
 	b1 = (b1-0.5)*chroma + l
-	r = scaleTo16Bit(r1)
-	g = scaleTo16Bit(g1)
-	b = scaleTo16Bit(b1)
+	r = uint32(scaleTo16Bit(r1))
+	g = uint32(scaleTo16Bit(g1))
+	b = uint32(scaleTo16Bit(b1))
 	a = 0xffff
 	return
 }
 
 type HSLImage struct {
-	pixels []HSLColor
+	// We'll keep the HSL pixel data in a single slice to avoid any possible
+	// padding if we use a slice of color structs instead. (This is why
+	// HSLColor is a slice, rather than a struct.)
+	pixels []uint16
 	w, h   int
 }
 
@@ -113,17 +120,18 @@ func (h *HSLImage) ColorModel() color.Model {
 	return color.RGBA64Model
 }
 
-// Returns the index into h's pixel array corresponding to the pixel at x, y,
-// or 0 if x, y is outside of the image's bounds.
-func (h *HSLImage) safeIndex(x, y int) int {
+// Returns the HSLColor corresponding to the pixel at (x, y), or a separate,
+// black, HSLColor if the coordinate is outside of the image boundaries.
+func (h *HSLImage) HSLPixel(x, y int) HSLColor {
 	if (x < 0) || (y < 0) || (x >= h.w) || (y >= h.h) {
-		return 0
+		return HSLColor([]uint16{0, 0, 0})
 	}
-	return y*h.w + x
+	i := y*h.w + x
+	return HSLColor(h.pixels[i : i+3])
 }
 
 func (h *HSLImage) At(x, y int) color.Color {
-	return h.pixels[h.safeIndex(x, y)]
+	return h.HSLPixel(x, y)
 }
 
 // Takes another image and sets a component of each of this image's pixels
@@ -131,18 +139,18 @@ func (h *HSLImage) At(x, y int) color.Color {
 // larger than h in either dimension. Requires a "setter" function, which will
 // be called to obtain the updated value of each HSL pixel.
 func (h *HSLImage) SetComponent(pic image.Image,
-	setter func(HSLColor, color.Color) HSLColor) {
+	setter func(HSLColor, color.Color)) {
 	bounds := pic.Bounds().Canon()
 	localX := 0
 	localY := 0
 	for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
 		localX = 0
 		for x := bounds.Min.X; x < bounds.Max.X; x++ {
-			i := h.safeIndex(localX, localY)
+			hslPixel := h.HSLPixel(localX, localY)
 			// Here is where we call the provided "setter" function to update
 			// the components of h.pixels[i]. In practice, setter will always
 			// be setH, setS, or setL.
-			h.pixels[i] = setter(h.pixels[i], pic.At(x, y))
+			setter(hslPixel, pic.At(x, y))
 			localX++
 		}
 		localY++
@@ -152,10 +160,12 @@ func (h *HSLImage) SetComponent(pic image.Image,
 // "Rotates" the hue value of each pixel in the image forward by the given
 // amount.
 func (h *HSLImage) AdjustHue(adjustment float64) {
-	for i := range h.pixels {
-		oldHue := float64(h.pixels[i].H)
-		_, newHue := math.Modf(oldHue + adjustment)
-		h.pixels[i].H = float32(newHue)
+	for y := 0; y < h.h; y++ {
+		for x := 0; x < h.w; x++ {
+			hslPixel := h.HSLPixel(x, y)
+			// We'll just let this wrap around to take care of the rotation.
+			hslPixel[0] += scaleTo16Bit(adjustment)
+		}
 	}
 }
 
@@ -166,7 +176,7 @@ func newHSLImage(w, h int) (*HSLImage, error) {
 	return &HSLImage{
 		w:      w,
 		h:      h,
-		pixels: make([]HSLColor, w*h),
+		pixels: make([]uint16, 3*w*h),
 	}, nil
 }
 
@@ -225,7 +235,7 @@ func combineImages(imageFiles []string, adjustHue float64) (image.Image,
 
 	// We'll index into this array to get the setter function for each
 	// component of the HSL color.
-	setterFunctions := []func(HSLColor, color.Color) HSLColor{
+	setterFunctions := []func(HSLColor, color.Color){
 		setH,
 		setS,
 		setL,
